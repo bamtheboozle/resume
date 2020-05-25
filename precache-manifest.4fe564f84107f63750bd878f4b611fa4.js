553e75afc2ff411262f508c3a3058d276bd8e73c
dragos.marinescu <dragos.marinescu@aeratechnology.com>
2020-05-25 16:26:10 +0300

    Updates

---
+++ precache-manifest.4fe564f84107f63750bd878f4b611fa4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9914bac9674452a5819377bc3e56048",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "60c5b31de0b62235b415",
+    "url": "/resume/static/css/main.ad51c871.chunk.css"
+  },
+  {
+    "revision": "58c1888b1c50d8b6e1b7",
+    "url": "/resume/static/js/2.06f99fb5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/resume/static/js/2.06f99fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c5b31de0b62235b415",
+    "url": "/resume/static/js/main.8be3c42f.chunk.js"
+  },
+  {
+    "revision": "a4cfb6a4e68719b206b0",
+    "url": "/resume/static/js/runtime-main.32d62fa2.js"
+  },
+  {
+    "revision": "893cb4b2845eefac7e0a4298e360fee2",
+    "url": "/resume/static/media/BigBlue.893cb4b2.ttf"
+  },
+  {
+    "revision": "bb606360beac956507d2d6079771730e",
+    "url": "/resume/static/media/clock.bb606360.svg"
+  }
+]);
